61ba703ad0f09611d7c9fbfd7fd8881696b66425
okibe <okibebernard@gmail.com>
2024-07-05 07:38:09 +0100

    initial commit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -54,6 +54,7 @@ const Form = (props) => {
                 name='firstName'
                 onChange={onChangeHandler}
                 value={formData.firstName}
+                required
                 className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
               />
             </div>
@@ -69,6 +70,7 @@ const Form = (props) => {
                 name='lastName'
                 onChange={onChangeHandler}
                 value={formData.lastName}
+                required
                 className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
               />
             </div>
@@ -84,6 +86,7 @@ const Form = (props) => {
                 type='email'
                 onChange={onChangeHandler}
                 value={formData.email}
+                required
                 className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
               />
             </div>
@@ -99,6 +102,7 @@ const Form = (props) => {
                 name='mobileNumber'
                 onChange={onChangeHandler}
                 value={formData.mobileNumber}
+                required
                 className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
               />
             </div>
@@ -160,7 +164,7 @@ const Form = (props) => {
           </div>
 
           <div className='sm:col-span-6 flex'>
-            <input className='ring-gray-600 mx-2' type='checkbox' />
+            <input className='ring-gray-600 mx-2' type='checkbox' required />
             <p>I agree to Beauty with Sharon </p>
             <p className='mx-2 underline'>privacy policy</p>
           </div>
@@ -173,7 +177,7 @@ const Form = (props) => {
             appointment
           </p>
           <div className='flex'>
-            <input className='ring-grayz-600 mr-2' type='checkbox' />
+            <input className='ring-grayz-600 mr-2' type='checkbox' required />
             <p className="text-sm font-medium'"> I agree</p>
           </div>
         </div>
